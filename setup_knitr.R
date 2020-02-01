ac388e5c16422fb542fddb898b7d22a6662b3a0e
Fernando Mayer <fernandomayer@gmail.com>
2020-01-31 22:14:42 -0300

    Arruma configuracoes gerais

--- setup_knitr.R
+++ setup_knitr.R
@@ -1,18 +1,15 @@
 library(knitr)
 ## knitr options
 opts_chunk$set(
-    cache = TRUE,
+    cache = FALSE,
     tidy = FALSE,
-    comment = NA,
-    ## collapse = TRUE, ## colapsa chunks em R Markdown
-    ## fig.width = 10,
-    ## fig.height = 8,
+    comment = "#",
+    collapse = TRUE, ## colapsa chunks em R Markdown
+    fig.width = 10,
+    fig.height = 8,
     fig.align = "center",
     ## dpi = 80, ## aumentar o dpi para exibir maior.
     ## dev = "png",
     fig.path = "figures/",
     cache.path = "cache/"
     )
-
-## Set a common seed for all files
-set.seed(100)
